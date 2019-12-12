6fa7f2bd217f6579283e745f22accf98fd84ff28
lim swee ming <ming08082010@hotmail.com>
2019-12-12 21:11:13 +0800

    [Module-2]-Drilling into Data

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() {
+                new Restaurant {Id = 1, Name = "Nasi Lemak", Location = "Selangor", Cuisine = CuisineType.KL},
+                new Restaurant {Id = 2, Name = "Tomyam", Location = "Genting", Cuisine = CuisineType.Pahang},
+                new Restaurant {Id = 3, Name = "Cendol", Location = "Geoge", Cuisine = CuisineType.Penang}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -22,6 +23,8 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+
             services.AddRazorPages();
         }
 
