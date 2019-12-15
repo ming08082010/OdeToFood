d2c0208868514638b03f1927c03827ce9df81b24
lim swee ming <ming08082010@hotmail.com>
2019-12-15 12:18:51 +0800

    [Module-7]-Integrating client-side js and css

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -31,6 +31,7 @@ namespace OdeToFood
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
 
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +48,8 @@ namespace OdeToFood
 
             app.UseStaticFiles();
 
+            app.UseNodeModules();
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -54,6 +57,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
